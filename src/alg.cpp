b5331fa1f560b83af9698f4933e338d8e1b89489
Шмыгленко Никита <99179542+Shmygam@users.noreply.github.com>
2022-04-03 18:28:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,6 +3,35 @@
 #include <map>
 #include "tstack.h"
 
+int priority(char symbol) {
+    switch (symbol) {
+    case '(': {
+        return 0;
+    }
+    case ')': {
+        return 1;
+    }
+    case '+': {
+        return 2;
+    }
+    case '-': {
+        return 2;
+    }
+    case '*': {
+        return 3;
+    }
+    case '/': {
+        return 3;
+    }
+    default: {
+        return 4;
+    }
+    case ' ': {
+        return 5;
+    }
+    }
+}
+
 std::string infx2pstfx(std::string inf) {
     TStack<char, 100> stack1;
     std::string output = "";
